e4b1f7286a7640b17b38092139df97e639798178
vsahu45147 <viswasahu2003@gmail>
2025-07-23 08:02:39 +0530

    fix: correct background colors in index.html and style.css

---
+++ day3/p1/p2/day 5/day 6 css box model/day7 css position property/day 8 css unit,percentage/day 9 css flexbox/day 10 css media queries/day11 data types in js/script for variables.js
@@ -0,0 +1,67 @@
+let username= 'viswa'
+console.log(username)
+console.log(typeof username)
+
+age = 25
+console.log(age)
+console.log(typeof age)
+
+let isHappy = true
+console.log(isHappy)
+console.log(typeof isHappy)
+
+let lastName = 'singh'
+console.log(lastName)
+console.log(typeof lastName)
+
+username = 'Shreya'
+username = 100
+console.log(username)
+console.log(typeof username)
+
+let userIntro = "Hii i am " + " " + username + " " + lastName + " " + "i am" + age + "year old"
+console.log(userIntro)
+
+let a = undefined
+console.log(a)
+console.log(typeof a)
+
+let b;
+console.log(b)
+console.log(typeof  b)
+
+b = false
+console.log(b, typeof b, 'xyz')
+
+let c = null
+console.log(c, typeof c)
+
+// <==========================================================================================>
+const hoursInADay = 24
+// hoursInADay = 20
+
+// const d;
+// <=========================================================================================>
+var e
+e = 10
+console.log(e)
+
+var f;
+console.log(f)
+
+{
+    var g = 20
+    var h = 30;
+}
+
+console.log(g)
+console.log(h)
+
+console.log(x)
+let x = 2020;
+console.log(x)//2020
+
+console.log('before initiliasation the value of y',y)//undeFined
+var y = 3020
+console.log(y)
+    
